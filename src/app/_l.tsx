a154d921ec9fdf119e1215123c7a3554e1e9824a
re-up-hq <hq@re-up.ph>
2025-10-23 16:26:02 +0800

    update:deploy-check

--- src/app/_l.tsx
+++
@@ -1,8 +0,0 @@
-import {Loader} from '@/components/loader'
-export default function Loading() {
-  return (
-    <div className='size-screen flex items-center justify-center'>
-      <Loader fullScreen />
-    </div>
-  )
-}
